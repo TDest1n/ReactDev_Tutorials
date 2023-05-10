f2852fe8c9409b8088120b215fbb90101065c86d
Terrance IvyBrown <boxman06@yahoo.com>
2023-05-10 03:28:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 // function MyButton() {
@@ -27,28 +28,28 @@ import "./styles.css";
 //   );
 // }
 
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "htt[s: //i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
+// const user = {
+//   name: "Hedy Lamarr",
+//   imageUrl: "htt[s: //i.imgur.com/yXOvdOSs.jpg",
+//   imageSize: 90,
+// };
 
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </>
-  );
-}
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={"Photo of " + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize,
+//         }}
+//       />
+//     </>
+//   );
+// }
 
 // Conditional Rendering
 // let content;
@@ -66,4 +67,40 @@ export default function Profile() {
   /* <div>{isLoggedIn ?(  <AdminPanel /> ) : ( <LoginForm />)}</div>; */
 }
 // Shorter and More Logical Syntax
-<div>{isLoggedIn && <AdminPanel />}</div>;
+{
+  /* <div>{isLoggedIn && <AdminPanel />}</div>; */
+}
+
+// Rendering Lists
+
+// const products = [
+//   { title: "Cabbage", isFruit: false, id: 1 },
+//   { title: "Garlic", isFruit: false, id: 2 },
+//   { title: "Apple", isFruit: true, id: 3 },
+// ];
+
+// export default function ShoppingList() {
+//   const listItems = products.map((product) => (
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? "magenta" : "darkgreen",
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   ));
+
+//   return <ul>{listItems}</ul>;
+// }
+// Responding to events
+
+// export default function MyButton() {
+//   function handleClick() {
+//     alert("You clicked Me!");
+//   }
+
+//   return <button onClick={handleClick}>Click Me</button>;
+// }
+
+// Updating the screen
