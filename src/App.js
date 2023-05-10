5005971ac4130c33572357f288bb5c6480096c95
Terrance IvyBrown <boxman06@yahoo.com>
2023-05-10 16:27:42 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -104,3 +104,28 @@ import "./styles.css";
 // }
 
 // Updating the screen
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+// }
+// function handleClick() {
+//   setCount(count + 1);
+// }
+// return <button onClick={handleClick}>Clicked {count} times</button>;
+
+export default function MyApp() {
+  return (
+    <div>
+      Counters that update seperately
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+
+  function MyButton() {
+    const [count, setCount] = useState(0);
+  }
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
